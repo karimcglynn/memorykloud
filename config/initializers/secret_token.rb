5c481944ae14817c9d6e8893c6f72aa1a316baba
MemoryKloud <MemoryKloud@gmail.com>
2011-11-11 15:33:07 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memorykloud::Application.config.secret_token = '3f0ba767adcbf99225736ec6078fd2422fb664e7cf7e9431f0eb1ed36728e3c6521f88e77fb78641d73ecc16a299e0b6ae2f2c568154909f37017c5b4204cabf'
